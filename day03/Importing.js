416322fe8d26a89a106d6ab53abaf39a84f55ad9
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-09-08 10:29:26 -0400

    Day03

---
+++ day03/Importing.js
@@ -0,0 +1,18 @@
+/*
+const allUtility = require('./Utility.js');
+
+let result = allUtility.square(9);
+
+console.log(result);
+
+result = allUtility.cube(3);
+
+console.log(result);
+
+result = allUtility.calculate(10, 20, '+');
+
+console.log(result);
+*/
+
+
+
